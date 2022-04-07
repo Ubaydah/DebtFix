424b5479f22fd911e333a1fcd09c2912fe9e37fc
Ubaydah <abdulwasiuubaydah@gmail.com>
2022-04-07 12:06:06 +0100

    added name in url path

--- Backend/authentify/urls.py
+++ Backend/authentify/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import Register, Login
 
 urlpatterns = [
-    path('register/', Register.as_view()),
-    path('login/', Login.as_view()),
+    path("register/", Register.as_view(), name="register"),
+    path("login/", Login.as_view(), name="login"),
 ]

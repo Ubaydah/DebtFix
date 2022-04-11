98a57f9da32226f34c358d9a282e98599d1815e0
Ubaydah <abdulwasiuubaydah@gmail.com>
2022-04-11 18:10:34 +0100

    added generics

--- Backend/authentify/views.py
+++ Backend/authentify/views.py
@@ -195,7 +195,7 @@ class DepositFunds(CreateAPIView):
         return Response({"authorization_url": response}, status=status.HTTP_201_CREATED)
 
 
-class PaystackWebhookView(APIView):
+class PaystackWebhookView(generic.View):
     def post(self, request, *args, **kwargs):
         data = json.loads(request.body.decode("utf-8"))
 
